169d87e444816765765b004e3cce9afae92a9a79
a35189459 <a23539510@gmail.com>
2023-01-13 17:40:10 +0800

    first

---
+++ aboutUs.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>日。嚐 甜</title>
+    <link rel="stylesheet" href="./css/aboutUs.css" />
+    <!--<script language="javascript" src="./js/project1.js"></script>-->
+    
+    
+</head>
+<body>
+    <nav>
+        <!--頁首-->
+        <div id="header">
+            <div id="d1">
+                <div id="dd1">
+                    <img src="img/sun1.png" alt="logo"/>
+                </div>
+                <div id="dd2">
+                    <h1>日。嚐 甜</h1>
+                </div>
+               
+            </div>
+            
+            <!--選單列表-->
+            <div>
+                <ul id="d2">
+                    <li><a href="project1.php" >首頁</a></li>
+                    <li><a href="aboutUs.php" >關於我們</a></li>
+                    <li><a href="reserve.php" >我要訂購</a></li>
+                    <li><a href="branch.php" >分店據點</a></li>
+                    <li><a href="question.php" >常見問題</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="content">
+            <div style="padding:5% 12%;">
+                <p style="text-align: center; font-size: 2.5rem;">ᴀʙᴏᴜᴛ ᴜs...</p>
+                <br/>
+                <p>日。嚐甜，不是販售外觀裝飾很漂亮的蛋糕，而是單純的原料，搭配簡單的層次組合襯托出蛋糕細膩度</p>
+                <br/>
+                <p>像是在複雜的世界中，卸下心防 閒暇之餘 用心品嚐簡單卻又細膩的幸福，保有舒服自在的生活方式</p>
+                <br/>
+                <p style="margin: 0 auto;">日子簡單點，品嚐一份甜點 分享 擁有加倍的幸福。</p>
+                <hr/>
+                <img src="img/images.jpg" alt="us" width="30%"/>
+                <img src="img/a7.jpg" alt="us"width="30%"/>
+                <img src="img/a13.jpg" alt="us"width="30%"/>
+            </div>
+        </div>
+            
+
+        <footer>
+            <div class="ft1">
+                <div>
+                    <span>© Copyright 日。嚐甜 - All rights reserved.</span>
+                    <a href="aboutUS.php"><input  class="ft2" type="button" value="TOP" /></a>
+                </div>
+            </div>
+        </footer>
+    </nav>
+</body>
+</html>
